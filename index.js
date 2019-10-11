f743e804152184ecf441b6b7270c62967d649148
unknown <vladi00345@gmail.com>
2019-10-11 14:41:32 -0700

    Removed high frequency

--- index.js
+++ index.js
@@ -38,22 +38,24 @@ for(let i = 0; i < squares.length; i ++) {
 }
 
 setInterval(() => {
+    const octave = tuner.noteName ? tuner.noteName.slice(tuner.noteName.length - 1) : undefined;
     const note = tuner.noteName ? tuner.noteName.slice(0,1) : undefined;
     console.log(note);
-    if(note === "A") {
+    console.log(octave);
+    if(note === "A" && (octave !== "7" && octave !=="6")) {
       for(let i = 0; i < circles.length; i ++){
         circles[i].style.animationPlayState = "running";
       }
-    } else if(note === "B"){
+    } else if(note === "B" && (octave !== "7" && octave !=="6")){
       box.style.animationPlayState = "running";
-    } else if(note === "C"){
+    } else if(note === "C" && (octave !== "7" && octave !=="6")){
       for(let i = 0; i < doritos.length; i ++){
         doritos[i].classList.add("triangle-animated");
       }
-    } else if(note === "D"){
+    } else if(note === "D" && (octave !== "7" && octave !=="6")){
       for(let i = 0; i < squares.length; i ++){
         squares[i].style.animationPlayState = "running"
       }
     }
-}, 500);
+}, 250);
 // If you sing into your microphone, your pitch will be logged to the console in real time.
