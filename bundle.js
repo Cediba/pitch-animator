aabe27631cb1184a731c29a898128955fa9fc428
unknown <vladi00345@gmail.com>
2019-10-11 00:32:05 -0700

    IDK

--- bundle.js
+++ bundle.js
@@ -15,45 +15,6 @@ setInterval(() => {
 }, 500);
 // If you sing into your microphone, your pitch will be logged to the console in real time.
 
-tuner.stopUpdatingPitch(); // Stop calculating the pitch if you don't need to know it anymore.
-
-// const Pitchfinder = require("pitchfinder");
-//
-// navigator.mediaDevices.getUserMedia({audio: true})
-//   .then(stream => {
-//       const mediaRecorder = new MediaRecorder(stream);
-//       mediaRecorder.start();
-//
-//       //collect audio data
-//       const audioChunks = [];
-//       mediaRecorder.addEventListener("dataavailable", event => {
-//           audioChunks.push(event.data);
-//       });
-//
-//       //convert data and play the audio
-//       mediaRecorder.addEventListener("stop", () => {
-//           const audioBlob = new Blob(audioChunks);
-//           const audioContext = new AudioContext();
-//           audioBlob.arrayBuffer()
-//           .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-//           .then(audioBuffer => {
-//             console.log(audioBuffer);
-//             const pitchfinder = Pitchfinder.YIN({sampleRate: 48000});
-//             const float32Array = audioBuffer.getChannelData(0);
-//             if(float32Array){
-//               console.log(float32Array);
-//               const pitch = pitchfinder(float32Array);
-//               console.log(pitch);
-//             }
-//           })
-//       });
-//
-//       //stop recording
-//       setTimeout(() => {
-//           mediaRecorder.stop();
-//       }, 1000);
-//     });
-
 },{"web-audio-daw":2}],2:[function(require,module,exports){
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
